4c51b8e5394bb04dfe86fa76cd6794e7bcdaf2f4
Christopher Altig <chris.altig@gmail.com>
2019-12-30 20:15:00 -0800

    slight naming adjustment for connection string

--- src/Common.fs
+++ src/Common.fs
@@ -4,10 +4,14 @@ module DogPark.Common
 open System.IO
 open Markdig
 
+#if DEBUG
+Directory.SetCurrentDirectory("bin/Debug/netcoreapp3.1/")
+#endif
+
 let contentRoot = Directory.GetCurrentDirectory()
 let webRoot     = Path.Combine(contentRoot, "WebRoot")
 let articleRoot = Path.Combine(contentRoot, "DogPark-Articles")
 
 let markdownPipeline = MarkdownPipelineBuilder().DisableHtml().Build()
 
-let [<Literal>] mdbConnectionString = """Server=localhost;Uid=DogPark;Database=DogPark;Port=3306"""
+let [<Literal>] MDBConnectionString = """Server=localhost;Uid=DogPark;Database=DogPark;Port=3306"""
