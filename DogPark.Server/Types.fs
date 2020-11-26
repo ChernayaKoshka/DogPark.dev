8447885bc341728694442f5e7eac4aaeb57d791c
FatherFoxxy <rattusunison@gmail.com>
2020-11-25 17:14:53 -0800

    Add Article/ShortUrl types

--- DogPark.Server/Types.fs
+++ DogPark.Server/Types.fs
@@ -1,3 +1,18 @@
 [<AutoOpen>]
 module DogPark.Types
 
+type Article =
+    {
+        IDArticle: int
+        IDUser: int
+        Headline: string
+        FilePath: string
+    }
+
+type ShortUrl =
+    {
+        IDShortUrl: int
+        IDUser: int
+        Short: string
+        Long: string
+    }
