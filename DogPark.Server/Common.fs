250afedcff86c9b1fccb570bf1c3446dc594075f
FatherFoxxy <rattusunison@gmail.com>
2020-11-26 23:01:15 -0800

    Store articles in webroot

--- DogPark.Server/Common.fs
+++ DogPark.Server/Common.fs
@@ -10,7 +10,7 @@ let contentRoot     = AppContext.BaseDirectory
 let logRoot         = Path.Combine(contentRoot, "logs")
 let webRoot         = Path.Combine(contentRoot, "wwwroot")
 let blazorFramework = Path.Combine(webRoot, "_framework")
-let articleRoot     = Path.Combine(contentRoot, "articles")
+let articleRoot     = Path.Combine(webRoot, "articles")
 
 let markdownPipeline = MarkdownPipelineBuilder().DisableHtml().Build()
 let rand = Random()
