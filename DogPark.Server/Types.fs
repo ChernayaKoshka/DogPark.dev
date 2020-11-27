0533967ff7e57ff99156749b46e383c7be768e48
FatherFoxxy <rattusunison@gmail.com>
2020-11-26 18:34:21 -0800

    Allow User/Role to be null, add LoginModel, fix Article key type

--- DogPark.Server/Types.fs
+++ DogPark.Server/Types.fs
@@ -1,20 +1,28 @@
 [<AutoOpen>]
 module DogPark.Types
 
+[<AllowNullLiteral>]
 type User() =
     member val IDUser = -1 with get, set
     member val UserName : string = null with get, set
     member val NormalizedUserName : string = null with get, set
     member val PasswordHash : string = null with get, set
 
+[<AllowNullLiteral>]
 type Role() =
     member val IDRole = -1 with get, set
     member val Name = "ERROR" with get, set
     member val NormalizedName = "ERROR" with get, set
 
+type LoginModel =
+    {
+        Username: string
+        Password: string
+    }
+
 type Article =
     {
-        IDArticle: int
+        IDArticle: uint32
         IDUser: int
         Headline: string
         FilePath: string
