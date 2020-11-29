2f08c3d1e254b24b31e38f8cf4aba57051e9d1a7
FatherFoxxy <rattusunison@gmail.com>
2020-11-28 21:01:16 -0800

    Add HttpClient to services

--- DogPark.Client/Startup.fs
+++ DogPark.Client/Startup.fs
@@ -2,6 +2,7 @@ namespace DogPark.Client
 
 open Microsoft.AspNetCore.Components.WebAssembly.Hosting
 open Bolero.Remoting.Client
+open Microsoft.Extensions.DependencyInjection
 
 module Program =
 
@@ -10,5 +11,6 @@ module Program =
         let builder = WebAssemblyHostBuilder.CreateDefault(args)
         builder.RootComponents.Add<Main.MyApp>("#main")
         builder.Services.AddRemoting(builder.HostEnvironment) |> ignore
+        builder.Services.AddHttpClient() |> ignore
         builder.Build().RunAsync() |> ignore
         0
