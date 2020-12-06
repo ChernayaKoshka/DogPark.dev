6cc864a79eec78a43a18afb638947173cf7d8830
FatherFoxxy <rattusunison@gmail.com>
2020-12-06 10:14:48 -0800

    Add response types and some helpers

--- DogPark.Shared/Types.fs
+++ DogPark.Shared/Types.fs
@@ -9,12 +9,21 @@ type ChangePasswordModel =
         OldPassword: string
         NewPassword: string
     }
+    with
+        static member Default =
+            { OldPassword = String.Empty; NewPassword = String.Empty }
 
 type LoginModel =
     {
         Username: string
         Password: string
     }
+    with
+        static member Default =
+            { Username = String.Empty; Password = String.Empty }
+        member this.IsValid() =
+            let isFieldVald s = not (String.IsNullOrWhiteSpace(s) || s.Contains(" "))
+            isFieldVald this.Username && isFieldVald this.Password
 
 type ArticleDetails =
     {
@@ -30,4 +39,22 @@ type Article =
         Details: ArticleDetails
         Body: string
         HtmlBody: string
+    }
+
+type GenericResponse =
+    {
+        Success: bool
+        Message: string
+    }
+
+type AccountDetails =
+    {
+        Username: string
+    }
+
+type AccountDetailsResponse =
+    {
+        Success: bool
+        Details: AccountDetails option
+        Message: string option
     }
