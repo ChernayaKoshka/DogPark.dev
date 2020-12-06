41a1135373d4282bbe82ce7923253f4113160b93
FatherFoxxy <rattusunison@gmail.com>
2020-12-06 10:21:33 -0800

    terrible initial implementation of login/logout

--- DogPark.Client/Main.fs
+++ DogPark.Client/Main.fs
@@ -27,6 +27,20 @@ type Api =
         [<Method("GET")>]
         [<Path("article/{Id}")>]
         Article: {| Id: uint32 |} -> Task<Article>
+
+        [<Path("account/login")>]
+        Login: LoginModel -> Task<AccountDetailsResponse>
+
+        [<Path("account/logout")>]
+        Logout: unit -> Task<GenericResponse>
+
+        [<Path("account/changepassword")>]
+        ChangePassword: ChangePasswordModel -> Task<GenericResponse>
+
+        [<Method("GET")>]
+        [<Path("account/details")>]
+        AccountDetails: unit -> Task<AccountDetailsResponse>
+
     }
     with
         #if DEBUG
@@ -39,6 +53,7 @@ type Page =
     | Home
     | Article of id: uint32
     | Articles
+    | Login
 
 type Model =
     {
@@ -49,15 +64,29 @@ type Model =
 
         ArticlesList: ArticleDetails seq
         Article: Article option
+        Login: LoginModel option
+        AccountDetails: AccountDetails option
     }
 type Message =
     | SetPage of Page
     | Ping
     | SetPingText of string
+
+    | GetAccountDetails
+    | GotAccountDetails of AccountDetailsResponse
+
     | GetArticlesList
     | GotArticlesList of ArticleDetails seq
     | GetArticle of id: uint32
     | GotArticle of Article
+
+    | Login
+    | LoginResult of AccountDetailsResponse
+    | Logout
+    | LogoutResult of GenericResponse
+
+    | SetUsername of string
+    | SetPassword of string
     | DoNothing
 
 let router = Router.infer SetPage (fun m -> m.Page)
@@ -66,9 +95,10 @@ let initModel (clientFactory: IHttpClientFactory) =
     let client = clientFactory.CreateClient(BaseAddress = Api.BaseUri)
 
     let api =
-        match makeApi<Api> Api.BaseUri client with
+        match makeApi<Api> Api.BaseUri jsonOptions client with
         | Ok api -> api
         | Error err -> failwith err
+
     {
         Page = Home
         Api = api
@@ -76,7 +106,9 @@ let initModel (clientFactory: IHttpClientFactory) =
         PingResult = "Press the Ping! button"
         ArticlesList = Seq.empty
         Article = None
-    }, Cmd.none
+        Login = None
+        AccountDetails = None
+    }, Cmd.ofMsg GetAccountDetails
 
 let update message model =
     match message with
@@ -98,6 +130,19 @@ let update message model =
         }, Cmd.none
     | Ping ->
         model, Cmd.OfTask.perform model.Api.Ping () SetPingText
+
+    | GetAccountDetails ->
+        model,
+        Cmd.OfTask.either
+            model.Api.AccountDetails
+            ()
+            GotAccountDetails
+            (fun err -> printfn "%A" err; DoNothing)
+    | GotAccountDetails details ->
+        { model with
+            AccountDetails = details.Details
+        }, Cmd.none
+
     | GetArticlesList ->
         model,
         Cmd.OfTask.either
@@ -120,12 +165,85 @@ let update message model =
         { model with
             Article = Some article
         }, Cmd.none
-    | DoNothing -> model, Cmd.none
+
+    | Login ->
+        match model.Login with
+        | Some login ->
+            model,
+            Cmd.OfTask.either
+                model.Api.Login
+                login
+                LoginResult
+                (fun err -> printfn "%A" err; DoNothing)
+        | None ->
+            model, Cmd.none
+    | LoginResult result ->
+        printfn "%A" result
+        { model with
+            AccountDetails = result.Details
+        }, Cmd.ofMsg (SetPage Home)
+    | Logout ->
+        model,
+        Cmd.OfTask.either
+            model.Api.Logout
+            ()
+            LogoutResult
+            (fun err -> printfn "%A" err; DoNothing)
+    | LogoutResult result ->
+        if result.Success then
+            { model with AccountDetails = None }, Cmd.none
+        else
+            printfn "%A" result
+            model, Cmd.none
+
+    | SetUsername username ->
+        let login =
+            match model.Login with
+            | Some login ->
+                { login with Username = username }
+            | None ->
+                { Username = username; Password = String.Empty }
+        { model with Login = Some login }, Cmd.none
+    | SetPassword password ->
+        let login =
+            match model.Login with
+            | Some login ->
+                { login with Password = password }
+            | None ->
+                { Username = String.Empty; Password = password }
+        { model with Login = Some login }, Cmd.none
+    | DoNothing ->
+        model, Cmd.none
 
 type View = Template<"./wwwroot/templates/realIndex.html">
 let articlesView model dispatch =
     View()
         .Head(Empty)
+        .LoginoutButtonLink(
+            if model.AccountDetails.IsSome then
+                null
+            else
+                router.Link Page.Login
+        )
+        .LoginoutButtonText(
+            if model.AccountDetails.IsSome then
+                "Logout"
+            else
+                "Login"
+        )
+        .LoginoutButtonClicked((fun _ ->
+            if model.AccountDetails.IsSome then
+                dispatch Logout
+            )
+        )
+        .HelloText(
+            match model.AccountDetails with
+            | Some details ->
+                p [ attr.``class`` "navbar-item is-size-5" ] [
+                    text $"Hello, {details.Username}"
+                ]
+            | None -> Empty
+        )
         .Content(
             div [ ] [
                 ul [ ] [
@@ -147,6 +265,31 @@ type Article = Template<"./wwwroot/templates/article.html">
 let articleView model dispatch =
     View()
         .Head(link [ attr.rel "stylesheet"; attr.href "css/vs.css" ])
+        .LoginoutButtonLink(
+            if model.AccountDetails.IsSome then
+                null
+            else
+                router.Link Page.Login
+        )
+        .LoginoutButtonText(
+            if model.AccountDetails.IsSome then
+                "Logout"
+            else
+                "Login"
+        )
+        .LoginoutButtonClicked((fun _ ->
+            if model.AccountDetails.IsSome then
+                dispatch Logout
+            )
+        )
+        .HelloText(
+            match model.AccountDetails with
+            | Some details ->
+                p [ attr.``class`` "navbar-item is-size-5" ] [
+                    text $"Hello, {details.Username}"
+                ]
+            | None -> Empty
+        )
         .Content(
             div [ ] [
                 cond model.Article <| function
@@ -183,12 +326,96 @@ let articleView model dispatch =
 let homeView model dispatch =
     View()
         .Head(Empty)
+        .LoginoutButtonLink(
+            if model.AccountDetails.IsSome then
+                null
+            else
+                router.Link Page.Login
+        )
+        .LoginoutButtonText(
+            if model.AccountDetails.IsSome then
+                "Logout"
+            else
+                "Login"
+        )
+        .LoginoutButtonClicked((fun _ ->
+            if model.AccountDetails.IsSome then
+                dispatch Logout
+            )
+        )
+        .HelloText(
+            match model.AccountDetails with
+            | Some details ->
+                p [ attr.``class`` "navbar-item is-size-5" ] [
+                    text $"Hello, {details.Username}"
+                ]
+            | None -> Empty
+        )
         .Content(
             div [ ] [
                 p [ ] [ text "Hello, world!" ]
                 text $"Ping Result: {model.PingResult}"
                 p [ ] [ button [ on.click (fun _ -> printfn "Dispatching"; dispatch Ping); ] [ text "Ping!" ] ]
             ]
+        )
+        .Scripts(Empty)
+        .Elt()
+
+type LoginView = Template<"./wwwroot/templates/login.html">
+let loginView model dispatch =
+    let login = Option.defaultValue LoginModel.Default model.Login
+
+    (*
+        <button
+            type="submit"
+            class="button is-primary"
+            onclick="${Login}"
+            ${LoginDisabled}>Login</button>
+    *)
+    let loginButton =
+        button [
+            attr.``type`` "submit"
+            attr.``class`` "button is-primary is-pulled-right"
+            on.click (fun _ -> dispatch Login)
+            if login.IsValid() |> not then attr.disabled ""
+        ] [
+            text "Login"
+        ]
+
+    View()
+        .Head(Empty)
+        .LoginoutButtonLink(
+            if model.AccountDetails.IsSome then
+                null
+            else
+                router.Link Page.Login
+        )
+        .LoginoutButtonText(
+            if model.AccountDetails.IsSome then
+                "Logout"
+            else
+                "Login"
+        )
+        .LoginoutButtonClicked((fun _ ->
+            if model.AccountDetails.IsSome then
+                dispatch Logout
+            )
+        )
+        .HelloText(
+            match model.AccountDetails with
+            | Some details ->
+                p [ attr.``class`` "navbar-item is-size-5" ] [
+                    text $"Hello, {details.Username}"
+                ]
+            | None -> Empty
+        )
+        .Content(
+            LoginView()
+                .Username(login.Username, (fun s -> dispatch (SetUsername s)))
+                .Password(login.Password, (fun s -> dispatch (SetPassword s)))
+                .Keyup(fun kea -> if kea.Key = "Enter" && login.IsValid() then dispatch Login)
+                .LoginButton(loginButton)
+                .Elt()
         )
         .Scripts(Empty)
         .Elt()
@@ -201,6 +428,8 @@ let view model dispatch =
         articleView model dispatch
     | Articles ->
         articlesView model dispatch
+    | Page.Login ->
+        loginView model dispatch
 
 type MyApp() =
     inherit ProgramComponent<Model, Message>()
