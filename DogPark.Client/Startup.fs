87f33ae2ef01ad5087cfad9548b8bd72bb53bcff
FatherFoxxy <rattusunison@gmail.com>
2020-12-08 22:01:41 -0800

    Add localstorage

--- DogPark.Client/Startup.fs
+++ DogPark.Client/Startup.fs
@@ -3,6 +3,7 @@ namespace DogPark.Client
 open Microsoft.AspNetCore.Components.WebAssembly.Hosting
 open Bolero.Remoting.Client
 open Microsoft.Extensions.DependencyInjection
+open Blazored.LocalStorage
 
 module Program =
 
@@ -12,5 +13,6 @@ module Program =
         builder.RootComponents.Add<Main.MyApp>(":root")
         builder.Services.AddRemoting(builder.HostEnvironment) |> ignore
         builder.Services.AddHttpClient() |> ignore
+        builder.Services.AddBlazoredLocalStorage() |> ignore
         builder.Build().RunAsync() |> ignore
         0
