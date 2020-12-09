7055d2cdd5f3add79aebcfb2fcf79756ac6ca42b
FatherFoxxy <rattusunison@gmail.com>
2020-12-08 20:03:43 -0800

    Add jwtDecodeNoVerify to Shared

--- DogPark.Shared/Shared.fs
+++ DogPark.Shared/Shared.fs
@@ -6,3 +6,7 @@ open System.Text.Json.Serialization
 
 let jsonOptions = JsonSerializerOptions()
 jsonOptions.Converters.Add(JsonFSharpConverter())
+
+let jwtDecodeNoVerify jwt =
+    let handler = System.IdentityModel.Tokens.Jwt.JwtSecurityTokenHandler()
+    handler.ReadJwtToken(jwt)
