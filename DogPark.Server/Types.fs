facdf179bc1522c35e035c965ec66bce340da0a0
FatherFoxxy <rattusunison@gmail.com>
2020-12-14 21:48:15 -0800

    Change ids to uint32

--- DogPark.Server/Types.fs
+++ DogPark.Server/Types.fs
@@ -4,14 +4,14 @@ open System
 
 [<AllowNullLiteral>]
 type User() =
-    member val IDUser = -1 with get, set
+    member val IDUser = 0u with get, set
     member val UserName : string = null with get, set
     member val NormalizedUserName : string = null with get, set
     member val PasswordHash : string = null with get, set
 
 [<AllowNullLiteral>]
 type Role() =
-    member val IDRole = -1 with get, set
+    member val IDRole = 0u with get, set
     member val Name = "ERROR" with get, set
     member val NormalizedName = "ERROR" with get, set
 
